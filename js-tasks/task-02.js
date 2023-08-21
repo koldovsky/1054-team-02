91826ad4bfbebdd6c56002d64a80b0c6b22d1483
Oksana Ivashchenko <113476406+OksanaIvash@users.noreply.github.com>
2023-08-21 09:17:58 +0000

    Add homework: advanced tasks

--- js-tasks/task-02.js
+++ js-tasks/task-02.js
@@ -24,3 +24,24 @@ function min(arr, toReturn) {
 function maps(x) {
     return x.map((n) => n * 2);
 }
+
+https://www.codewars.com/kata/a-wolf-in-sheeps-clothing/train/javascript
+function warnTheSheep(queue) {
+    queue.reverse();
+    const wolfIndex = queue.indexOf('wolf');
+    if (wolfIndex === 0) {
+      return 'Pls go away and stop eating my sheep';
+    } else {
+      return `Oi! Sheep number ${wolfIndex}! You are about to be eaten by a wolf!`;
+    }
+  }
+
+https://www.codewars.com/kata/find-the-first-non-consecutive-number/train/javascript
+function firstNonConsecutive(arr) {
+    for (let i = 1; i < arr.length; i++) {
+      if (arr[i] - arr[i - 1] !== 1 && arr[i] - arr[i - 1] !== -1) {
+        return arr[i];
+      }
+    }
+    return null;
+  }
