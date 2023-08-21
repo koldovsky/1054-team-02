836e7d6b3c02a048696fa04ad76b313ebe790be9
LuibomyrBorsch <140444137+LuibomyrBorsch@users.noreply.github.com>
2023-08-21 10:02:34 +0000

    add additional exercise

--- js-tasks/task-02.js
+++ js-tasks/task-02.js
@@ -25,7 +25,7 @@ function maps(x) {
   return x.map((n) => n * 2);
 }
 
-https://www.codewars.com/kata/a-wolf-in-sheeps-clothing/train/javascript
+//https://www.codewars.com/kata/a-wolf-in-sheeps-clothing/train/javascript
 function warnTheSheep(queue) {
   queue.reverse();
   const wolfIndex = queue.indexOf('wolf');
@@ -36,7 +36,7 @@ function warnTheSheep(queue) {
   }
 }
 
-https://www.codewars.com/kata/find-the-first-non-consecutive-number/train/javascript
+//https://www.codewars.com/kata/find-the-first-non-consecutive-number/train/javascript
 function firstNonConsecutive(arr) {
   for (let i = 1; i < arr.length; i++) {
     if (arr[i] - arr[i - 1] !== 1 && arr[i] - arr[i - 1] !== -1) {
@@ -46,17 +46,55 @@ function firstNonConsecutive(arr) {
   return null;
 }
 
-//Додаткові задачки:
-//Double Integer
+//Додаткові задачі:
 //https://www.codewars.com/kata/53ee5429ba190077850011d4/train/javascript
 const doubleInteger = (i) => 2 * i;
 
-//Century From Year 
 // https://www.codewars.com/kata/5a3fe3dde1ce0e8ed6000097
 const century = (year) => Math.ceil(year / 100);
 
-//Make negative
 //https://www.codewars.com/kata/55685cd7ad70877c23000102/train/javascript
 function makeNegative(num) {
   return num === 0 ? 0 : -Math.abs(num);
+}
+
+//https://www.codewars.com/kata/55fab1ffda3e2e44f00000c6/train/javascript
+function cockroachSpeed(s) {
+  return Math.floor(s * 100000 / 3600);
+}
+
+//https://www.codewars.com/kata/5a023c426975981341000014/train/javascript
+function otherAngle(a, b) {
+  return 180-(a+b);
+}
+
+//https://www.codewars.com/kata/5b853229cfde412a470000d0/train/javascript
+function twiceAsOld(dadYearsOld, sonYearsOld) {
+  return Math.abs(sonYearsOld * 2 - dadYearsOld);
+}
+
+//https://www.codewars.com/kata/5933a1f8552bc2750a0000ed/train/javascript
+function nthEven(n) {
+  return (n-1) * 2;
+}
+
+//https://www.codewars.com/kata/574b3b1599d8f897470018f6/train/javascript
+const getRealFloor = (n) => {
+  if (n >= 13) return n - 2;
+  if (n > 0) return n - 1;
+  return n;
+}
+
+//https://www.codewars.com/kata/55f9bca8ecaa9eac7100004a/train/javascript
+function past(h, m, s) {
+  const hours = h * 60 * 60 * 1000;
+  const minutes = m * 60 * 1000;
+  const seconds = s * 1000;
+  
+  return hours + minutes + seconds;
+}
+
+//https://www.codewars.com/kata/5545f109004975ea66000086/train/javascript
+function isDivisible(n, x, y) {
+  return n % x === 0 && n % y === 0;
 }
