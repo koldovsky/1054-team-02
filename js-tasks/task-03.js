a39eba01e95ec9dde8c377f6964999fde535a2bc
Oksana Ivashchenko <113476406+OksanaIvash@users.noreply.github.com>
2023-08-23 11:38:23 +0000

    Add a third task

--- js-tasks/task-03.js
+++ js-tasks/task-03.js
@@ -5,3 +5,17 @@ class SmallestIntegerFinder {
 
 // https://www.codewars.com/kata/geometry-basics-circle-circumference-in-2d/train/javascript
 circleCircumference = circle => 2 * Math.PI * circle.radius; // Аня
+
+//https://www.codewars.com/kata/training-js-number-12-loop-statement-for-dot-in-and-for-dot-of/train/javascript
+function giveMeFive(obj) {
+  const arr = [];
+  for (let key in obj) {
+    if (key.length === 5) {
+      arr.push(key);
+    }
+    if (obj[key].length === 5) {
+      arr.push(obj[key]);
+    }
+  }
+  return arr;
+}
