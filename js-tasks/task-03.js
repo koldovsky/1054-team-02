a9d432780b9073e1241fc49c1e4ca53123b7981f
Akornafel21 <125659495+Akornafel21@users.noreply.github.com>
2023-08-23 09:08:10 +0000

    Added file 'task-03' and two solutions

---
+++ js-tasks/task-03.js
@@ -0,0 +1,7 @@
+// https://www.codewars.com/kata/find-the-smallest-integer-in-the-array/train/javascript
+class SmallestIntegerFinder {
+    findSmallestInt = args => Math.min(...args)
+  } // Аня
+
+// https://www.codewars.com/kata/geometry-basics-circle-circumference-in-2d/train/javascript
+circleCircumference = circle => 2 * Math.PI * circle.radius; // Аня
