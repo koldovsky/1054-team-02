5f46acb3ed898907cffebbc6526fae8138923e61
Akornafel21 <125659495+Akornafel21@users.noreply.github.com>
2023-08-23 14:05:48 +0000

    Edited solutions task-03

--- js-tasks/task-03.js
+++ js-tasks/task-03.js
@@ -1,10 +1,10 @@
 // https://www.codewars.com/kata/find-the-smallest-integer-in-the-array/train/javascript
 class SmallestIntegerFinder {
     findSmallestInt = args => Math.min(...args)
-  } // Аня
+  } 
 
 // https://www.codewars.com/kata/geometry-basics-circle-circumference-in-2d/train/javascript
-circleCircumference = circle => 2 * Math.PI * circle.radius; // Аня
+circleCircumference = circle => 2 * Math.PI * circle.radius; 
 
 //https://www.codewars.com/kata/training-js-number-12-loop-statement-for-dot-in-and-for-dot-of/train/javascript
 function giveMeFive(obj) {
