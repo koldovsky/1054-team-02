9a2afb28cd79c1c8901179a5273df7fa9e362bae
Oksana Ivashchenko <studoks08@gmail.com>
2023-08-25 15:45:27 +0300

    Added function to burger menu file

--- js/burger-menu.js
+++ js/burger-menu.js
@@ -1,8 +1,6 @@
 (function () {
-    console.log ("Hello");
-    // const iconBurger = document.querySelector(".header__nav-burger");
-    // iconBurger.addEventListener("click", function (e) {
-    //     iconBurger.classList.toggle("active");
-
-    // });
+  const iconBurger = document.querySelector('.header__nav-burger');
+  iconBurger.addEventListener('click', function (e) {
+    iconBurger.classList.toggle('active');
+  });
 })();
