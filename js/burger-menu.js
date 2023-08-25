7b18acb08eb60dca288f65dd0a6427664afe3a1d
Oksana Ivashchenko <113476406+OksanaIvash@users.noreply.github.com>
2023-08-25 11:29:57 +0000

    Added a file js

---
+++ js/burger-menu.js
@@ -0,0 +1,8 @@
+(function () {
+    console.log ("Hello");
+    // const iconBurger = document.querySelector(".header__nav-burger");
+    // iconBurger.addEventListener("click", function (e) {
+    //     iconBurger.classList.toggle("active");
+
+    // });
+})();
