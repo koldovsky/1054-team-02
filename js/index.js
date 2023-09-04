8272c541d3cb1754ab40edd49ae8d1645dd786cb
Ivanna-Martynyshyn <139112772+Ivanna-Martynyshyn@users.noreply.github.com>
2023-09-04 05:55:51 +0000

    added latest news js

--- js/index.js
+++ js/index.js
@@ -2,4 +2,5 @@ document.addEventListener('partialsLoaded', () => {
     import('./burger-menu.js'); 
     import('./latest-news-clock.js');
     import('./win-animate.js');
+    import('./latest-news.js')
 });
