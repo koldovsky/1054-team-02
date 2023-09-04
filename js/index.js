1de93e891566145a5b86cc081eea178941da2cb8
IvanPiroman <141070516+IvanPiroman@users.noreply.github.com>
2023-09-04 15:55:02 +0000

    add js for schedule

--- js/index.js
+++ js/index.js
@@ -2,5 +2,6 @@ document.addEventListener('partialsLoaded', () => {
     import('./burger-menu.js'); 
     import('./latest-news-clock.js');
     import('./win-animate.js');
-    import('./latest-news.js')
+    import('./schedule-accordeon.js');
+    import('./latest-news.js');
 });
