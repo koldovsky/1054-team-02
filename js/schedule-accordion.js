95791b6244bdd1379a18b48ffcf90d9c5d668856
IvanPiroman <141070516+IvanPiroman@users.noreply.github.com>
2023-09-05 20:34:25 +0000

    change JS code for schedule

--- js/schedule-accordion.js
+++ js/schedule-accordion.js
@@ -1,15 +1,40 @@
-document.addEventListener("DOMContentLoaded", function () {
-    const toggles = document.querySelectorAll(".accordion-toggle");
+document.addEventListener('DOMContentLoaded', () => {
+  const rows = document.querySelectorAll('.schedule__row');
+  const contents = document.querySelectorAll('.accordion-content');
 
-    toggles.forEach((toggle) => {
-        toggle.addEventListener("click", function () {
-            const content = this.nextElementSibling;
-            if (window.getComputedStyle(content).display === "block") {
-                content.style.display = "none";
-            } else {
-                content.style.display = "block";
-            }
-            
-        });
+
+  function closeAllAccordions() {
+    contents.forEach((content) => {
+      content.style.display = 'none';
     });
+  }
+
+  
+  function addClickHandlersOnSmallScreens() {
+    if (window.innerWidth <= 768) {
+      rows.forEach((row, index) => {
+        row.addEventListener('click', () => {
+          closeAllAccordions();
+
+          
+          contents[index].style.display = 'block';
+        });
+      });
+    }
+  }
+
+  
+  addClickHandlersOnSmallScreens();
+
+  
+  window.addEventListener('resize', () => {
+    if (window.innerWidth <= 768) {
+      addClickHandlersOnSmallScreens();
+    } else {
+      
+      rows.forEach((row) => {
+        row.removeEventListener('click', closeAllAccordions);
+      });
+    }
+  });
 });
