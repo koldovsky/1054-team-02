9f03d0c9872c65c694375c99b80acfbc52c80e3f
IvanPiroman <141070516+IvanPiroman@users.noreply.github.com>
2023-09-05 21:07:13 +0000

    js for schedule

--- js/schedule-accordion.js
+++ js/schedule-accordion.js
@@ -1,40 +1,34 @@
-document.addEventListener('DOMContentLoaded', () => {
-  const rows = document.querySelectorAll('.schedule__row');
-  const contents = document.querySelectorAll('.accordion-content');
+// Отримуємо всі елементи з класом "accordion-toggle"
+const accordionToggles = document.querySelectorAll('.accordion-toggle');
 
-
-  function closeAllAccordions() {
-    contents.forEach((content) => {
-      content.style.display = 'none';
+// Додаємо обробник подій для кожного елемента "accordion-toggle"
+accordionToggles.forEach((toggle) => {
+  toggle.addEventListener('click', () => {
+    // Знаходимо батьківський елемент "schedule__row"
+    const row = toggle.closest('.schedule__row');
+    
+    // Знаходимо всі елементи "accordion-content" в межах батьківського елемента
+    const content = row.querySelectorAll('.accordion-content');
+    
+    // Перевіряємо, чи активний акордеон
+    const isActive = row.classList.contains('active');
+    
+    // Закриваємо всі активні акордеони
+    accordionToggles.forEach((otherToggle) => {
+      otherToggle.closest('.schedule__row').classList.remove('active');
     });
-  }
-
-  
-  function addClickHandlersOnSmallScreens() {
-    if (window.innerWidth <= 768) {
-      rows.forEach((row, index) => {
-        row.addEventListener('click', () => {
-          closeAllAccordions();
-
-          
-          contents[index].style.display = 'block';
-        });
+    
+    // Відкриваємо або закриваємо акордеон в залежності від стану
+    if (!isActive) {
+      content.forEach((item) => {
+        item.style.display = 'block';
       });
-    }
-  }
-
-  
-  addClickHandlersOnSmallScreens();
-
-  
-  window.addEventListener('resize', () => {
-    if (window.innerWidth <= 768) {
-      addClickHandlersOnSmallScreens();
+      row.classList.add('active');
     } else {
-      
-      rows.forEach((row) => {
-        row.removeEventListener('click', closeAllAccordions);
+      content.forEach((item) => {
+        item.style.display = 'none';
       });
+      row.classList.remove('active');
     }
   });
 });
