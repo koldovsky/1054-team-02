a772c275935478b4740cad9b39dde49d74ca9844
IvanPiroman <141070516+IvanPiroman@users.noreply.github.com>
2023-09-06 21:23:42 +0000

    schedule

--- js/index.js
+++ js/index.js
@@ -2,7 +2,7 @@ document.addEventListener('partialsLoaded', () => {
     import('./burger-menu.js'); 
     import('./latest-news-clock.js');
     import('./win-animate.js');
-    import('./schedule-accordion.js').then(module => module.default())
+    import('./schedule-accordion.js');
     import('./latest-news.js');
     import('./contact-form.js');
     import('./careers-animate.js');
--- js/schedule-accordion.js
+++ js/schedule-accordion.js
@@ -1,29 +1,24 @@
-export default function () {
-  document.addEventListener("DOMContentLoaded", function () {
-    var accordionToggles = document.querySelectorAll(".accordion-toggle");
+document.addEventListener("DOMContentLoaded", function () {
+  var accordionToggles = document.querySelectorAll(".accordion-toggle");
 
-    accordionToggles.forEach(function (toggle) {
-      toggle.addEventListener("click", function () {
-        var accordionContent = toggle.nextElementSibling;
+  accordionToggles.forEach(function (toggle) {
+    toggle.addEventListener("click", function () {
+      var accordionContent = toggle.nextElementSibling;
 
-        // Перевірка, чи елемент з таким класом існує
-        if (accordionContent) {
-          // Переключення між відображенням і приховуванням активного контенту
-          if (window.getComputedStyle(accordionContent).display === "none") {
-            accordionContent.style.display = "block";
-          } else {
-            accordionContent.style.display = "none";
-          }
-
-          // Переключення стрілки у заголовку акордеону
-          var arrow = toggle.querySelector(".accordion-toggle::after");
-          if (arrow.textContent === '\u25B6') {
-            arrow.textContent = '\u25BC';
-          } else {
-            arrow.textContent = '\u25B6';
-          }
+      if (accordionContent) {
+        if (window.getComputedStyle(accordionContent).display === "none") {
+          accordionContent.style.display = "block";
+        } else {
+          accordionContent.style.display = "none";
         }
-      });
+
+        var arrow = toggle.querySelector(".accordion-toggle::after");
+        if (arrow.textContent === '\u25B6') {
+          arrow.textContent = '\u25BC';
+        } else {
+          arrow.textContent = '\u25B6';
+        }
+      }
     });
   });
-}
+});
