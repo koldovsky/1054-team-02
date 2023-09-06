d5d0129c257a7445600ae5957843f9a9f6c5b501
Akornafel21 <125659495+Akornafel21@users.noreply.github.com>
2023-09-06 22:54:56 +0000

    Added minimal JS code vor about-us

---
+++ js/about-us.js
@@ -0,0 +1,21 @@
+const commentForm = document.getElementById('comment-form');
+const commentList = document.querySelector('.comment-list');
+
+commentForm.addEventListener('submit', function (e) {
+    e.preventDefault();
+
+    const name = document.getElementById('name').value;
+    const commentText = document.getElementById('comment').value;
+    const rating = parseInt(document.getElementById('rating').value);
+
+    // Створюємо новий коментар у вигляді HTML-елемента
+    const commentItem = document.createElement('li');
+    commentItem.innerHTML = `
+        <strong>${name}</strong> (Rating: ${rating}):<br>
+        ${commentText}
+    `;
+
+    commentList.appendChild(commentItem);
+
+    commentForm.reset();
+});
