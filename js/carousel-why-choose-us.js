3e9b56530c1e9cdc49560901ee3d51e1748ce276
tyrambar1 <110553765+tyrambar1@users.noreply.github.com>
2023-09-07 11:24:46 +0000

    add shows

---
+++ js/carousel-why-choose-us.js
@@ -0,0 +1,8 @@
+const slides = [
+    '<div><img src="img/" alt=""></div>',
+    '<div><img src="img/" alt=""></div>',
+    '<div><img src="img/" alt=""></div>',
+    '<div><img src="img/" alt=""></div>',
+];
+
+let currentSlide = 0;
